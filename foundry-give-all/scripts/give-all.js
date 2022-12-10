7f52b57f9b05a3398f568bd2d6166c7e73e3b4bb
Elliot Mitchell <e.mitchell@codat.io>
2022-12-10 19:46:06 +0000

    add right click option from the inventory page

--- foundry-give-all/scripts/give-all.js
+++ foundry-give-all/scripts/give-all.js
@@ -53,3 +53,42 @@ Hooks.on("getChatLog", (app, html, data) => {
         }
     });
 });
+
+// Define the UI
+Hooks.on("renderItemSheet", (app, html, data) => {
+    // Add the right-click option to the item sheet
+    html.find("button.item-control").last().after(`
+        <button class="item-control" data-item-name="${data.item.name}">
+            Give item to all players
+        </button>
+    `);
+
+    // Handle the button click
+    html.find("button[data-item-name]").click(async event => {
+        let itemName = $(event.currentTarget).data("itemName");
+        game.settings.set("my-addon", "itemName", itemName);
+
+        let chatData = {
+            user: game.user._id,
+            content: `Giving all players a ${itemName}...`
+        };
+
+        ChatMessage.create(chatData);
+
+        // Give all players an item
+        game.users.forEach(u => {
+            let actor = u.character;
+            if (!actor) return;
+
+            let item = actor.items.find(i => i.name === itemName);
+            if (item) {
+                item.increment();
+            } else {
+                actor.createOwnedItem({
+                    name: itemName
+                });
+            }
+        });
+    });
+});
+
